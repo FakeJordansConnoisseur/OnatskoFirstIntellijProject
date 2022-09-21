5585c033461f1f32341c21ca6b72e088e39930cf
1E10 Laptop <roberto58@nycstudents.net>
2022-09-21 10:38:09 -0400

    J's

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printlin("Hello World.");
+        System.out.println("I have acquired a fresh pair of fake jordans.");
     }
 }
